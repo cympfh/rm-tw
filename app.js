f526068674d3513a63cc76d35cb43fd0b8a15a0a
cympfh@NII <cympfh@gmail.com>
2014-02-14 21:33:50 +0900

    崩れ然る固定観念 迷いを抱きながら 其れでも尚 『自由』へ進め!!!

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-app.get('rm-tw', function(req, res) {
+app.get('/rm-tw', function(req, res) {
   res.send("Hello! Hello! Hello!")
 });
