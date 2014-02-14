e13bb22425038645f0ccc24b4d0beafd990d769b
cympfh@NII <cympfh@gmail.com>
2014-02-14 21:33:17 +0900

    大層な虚栄心に満たされるほうが怖い 描く景色を気安く壊すな

--- app.js
+++ app.js
@@ -34,3 +34,7 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+app.get('rm-tw', function(req, res) {
+  res.send("Hello! Hello! Hello!")
+});
